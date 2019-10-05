188aaf2b81ff974b4fbcc8bffa446cb1a8540022
Hector Valdez <hvaldez180@gmail.com>
2019-10-05 16:36:01 -0400

    added subscription button

---
+++ beauty/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import OutlinedButtons from "./components/Button";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <OutlinedButtons />
+    </div>
+  );
+}
+
+export default App;
